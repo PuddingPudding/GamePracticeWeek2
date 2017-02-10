c0825f42d0f58ceae7767e22a9409ff27415b32b
PuddingPudding <hwsh70119@gmail.com>
2017-02-10 17:13:33 +0800

    目前可用空白鍵跟狀態機溝通，間接讓Turrent動起來

---
+++ Assets/TurrentManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurrentManagerScript : MonoBehaviour
+{
+    private Animator _animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+    }
+
+    private void PlayShootAnimation()
+    {
+        _animator.SetTrigger("Shoot");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayShootAnimation();
+        }
+    }
+}
