6443c9303fae174d28c463d0705f6a38fd050403
PuddingPudding <hwsh70119@gmail.com>
2017-02-16 01:21:19 +0800

    調整子彈之飛行速度，現在子彈的飛行速度將會是固定的，且不會停下

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -6,7 +6,8 @@ public class BulletScript : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
     private SpriteRenderer _spriteRenderer;
-    private float _speed = 5;
+
+    private float _speed = 2;
 
 
     public void InitAndShoot(Vector2 direction)
@@ -27,6 +28,16 @@ public class BulletScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //Vector的normalized (一般化) 會維持你向量的比例，但是會把其長度變成1
+        if (_rigidbody2D.velocity == Vector2.zero)
+        {
+            //確保沒有人停下來
+            _rigidbody2D.velocity = new Vector2(Random.Range(0, 1.0f), Random.Range(0, 1.0f)).normalized * _speed;
+        }
+        else
+        {
+            //確保碰撞後速度不變
+            _rigidbody2D.velocity = _rigidbody2D.velocity.normalized * _speed;
+        }
     }
 }
